073c0954b350fd292fd3c26aa29fd71b918f576f
KranXD <167823362+KranXD@users.noreply.github.com>
2024-05-27 22:30:08 +0200

    Create counter.svelte

---
+++ src/lib/counter.svelte
@@ -0,0 +1,11 @@
+<script lang = "ts">
+  let count = 0;
+function addOne() {
+  count += 1;
+}
+</script>
+<style lang="scss">
+  
+</style>
+
+  <button on:click={addOne}>Clicked {count} times</button>
