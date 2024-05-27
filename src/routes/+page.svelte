ae6f34632670a2d969cb12c60b546045279224f2
KranXD <167823362+KranXD@users.noreply.github.com>
2024-05-27 21:10:21 +0200

    Update +page.svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+# Arduprojects
+
+Arduino projects by [KranXD](https://youtube.com/@KranXD_) and [Nito](https://youtube.com/@nito8066)
+
+# About us
+
+>[KranXD](https://youtube.com/@KranXD) is young and little bit bored arduino creator and linux > > user. He likes to sometime install [Arch linux](https://archlinux.org), Try new programming language, Or simply do nothing. 
+
+> [Nito](https://youtube.com/@nito8066) 
+
+# Projects
+- [WIP] [Game in Arduino](https://youtube.com/@KranXD_) by Kran
